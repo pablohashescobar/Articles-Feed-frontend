29877b248a2a2e246028776c375a63d8d587fc2c
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-04-26 16:22:40 +0530

    deployed server url

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -3,7 +3,7 @@ import { getUserToken } from "./Token";
 
 const ApiClient = () => {
   return axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://articles-feed-api.herokuapp.com/api",
     headers: {
       "x-auth-token": getUserToken(),
     },
