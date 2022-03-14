e149de74a2146ed60f8eeb85ebec551fc75f647a
nikhil1301z <nikhil@teachedison.com>
2022-03-14 22:54:42 +0530

    update APi Client

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -2,9 +2,8 @@ import axios from "axios";
 import { getUserToken } from "./Token";
 
 const ApiClient = () => {
-  console.log(process.env)
   return axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.NODE_ENV === "development" ? process.env.REACT_APP_API_URL : process.env.NEXT_PUBLIC_API_URL,
     headers: {
       "x-auth-token": getUserToken(),
     },
