bf134990f4f7eb8554630293678edf2a7c11aaf2
nikhil1301z <nikhil@teachedison.com>
2022-03-16 23:22:37 +0530

    dev: add sentry

--- src/index.js
+++ src/index.js
@@ -9,6 +9,22 @@ import { store, persistor } from "./utils/createStore";
 import { PersistGate } from "redux-persist/integration/react";
 import Loading from "./component/Loading";
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
